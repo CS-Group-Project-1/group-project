249d189c62b86ec7437886a457f7a39198e2839a
isabellaalbertoni <isabella.albertoni294@gmail.com>
2024-12-08 19:49:44 +0100

    created email credentials file

---
+++ email_credentials.py
@@ -0,0 +1,8 @@
+#In this file the email credentials can be found. We created an email address dedicated to the project.
+#as mentioned in the readme file, this email_credentials file is relevant in relation to the set up
+#of the notification system
+
+
+SMTP_EMAIL = "easy2trade1@gmail.com"  
+SMTP_PASSWORD = "jhnc mjmc srrt ictx" 
+ 
